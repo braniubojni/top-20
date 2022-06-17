c52d8a620f3e99c72319dc81275659944127f1ea
Erik <braniubojni@mail.ru>
2022-06-17 22:51:52 +0400

    chore(main): Successfully connected to db

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -10,17 +10,20 @@ export const getMongoConfig = async (
 	};
 };
 
-const getMongoString = (configService: ConfigService) =>
-	'mongodb://' +
-	configService.get('MONGO_LOGIN') +
-	':' +
-	configService.get('MONGO_PASSWORD') +
-	'@' +
-	configService.get('MONGO_HOST') +
-	':' +
-	configService.get('MONGO_PORT') +
-	'/' +
-	configService.get('MONGO_MONGO_AUTHDATABASE');
+const getMongoString = (configService: ConfigService) => {
+	return (
+		'mongodb://' +
+		configService.get('MONGO_LOGIN') +
+		':' +
+		configService.get('MONGO_PASSWORD') +
+		'@' +
+		configService.get('MONGO_HOST') +
+		':' +
+		configService.get('MONGO_PORT') +
+		'/' +
+		configService.get('MONGO_AUTHDATABASE')
+	);
+};
 
 const getMongoOptions = () => ({
 	useNewUrlParser: true,
