3b94c66d747edf8a354eb1eb59f75eb1772e0568
tagavor <erik95harutyunyan@gmail.com>
2022-08-21 20:13:53 +0400

    eh/feat: webp converter done

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,6 +1,7 @@
 import {
 	Controller,
 	HttpCode,
+	HttpStatus,
 	Post,
 	UploadedFile,
 	UseGuards,
@@ -9,6 +10,7 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { FilesService } from './files.service';
+import { MFile } from './mfile.class';
 import { FileElementResponse } from './response/file-element.response';
 
 @Controller('files')
@@ -16,12 +18,23 @@ export class FilesController {
 	constructor(private readonly filesService: FilesService) {}
 
 	@Post('upload')
-	@HttpCode(200)
+	@HttpCode(HttpStatus.OK)
 	@UseGuards(JwtAuthGuard)
 	@UseInterceptors(FileInterceptor('files'))
 	async uploadFile(
 		@UploadedFile() file: Express.Multer.File,
 	): Promise<FileElementResponse[]> {
-		return this.filesService.saveFiles([file]);
+		const saveArray = [new MFile(file)];
+		if (file.mimetype.includes('image')) {
+			const buffer = await this.filesService.convertToWebP(file.buffer);
+			// FIXME: This part looks ugly, make it dynamic not only for webp
+			saveArray.push(
+				new MFile({
+					originalname: `${file.originalname.split('.')[0]}.webp`,
+					buffer,
+				}),
+			);
+		}
+		return this.filesService.saveFiles(saveArray);
 	}
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,15 +3,17 @@ import { path } from 'app-root-path';
 import { format } from 'date-fns';
 import { ensureDir, writeFile } from 'fs-extra';
 import { FileElementResponse } from './response/file-element.response';
+import * as sharp from 'sharp';
+import { MFile } from './mfile.class';
 
 @Injectable()
 export class FilesService {
-	async saveFiles(
-		files: Express.Multer.File[],
-	): Promise<FileElementResponse[]> {
+	async saveFiles(files: MFile[]): Promise<FileElementResponse[]> {
 		const dateFolder = format(new Date(), 'yyyy-MM-dd');
 		const uploadFolder = `${path}/uploads/${dateFolder}`;
+
 		await ensureDir(uploadFolder);
+
 		const res: FileElementResponse[] = [];
 		for (const file of files) {
 			await writeFile(`${uploadFolder}/${file.originalname}`, file.buffer);
@@ -22,5 +24,10 @@ export class FilesService {
 		}
 
 		return res;
+	}
+
+	convertToWebP(file: Buffer): Promise<Buffer> {
+		// FIXME: In future deligate to separate service convert
+		return sharp(file).webp().toBuffer();
 	}
 }
