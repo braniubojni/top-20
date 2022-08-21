1a716d94e4db0409ecf7b749035bb4b67a290e8a
tagavor <erik95harutyunyan@gmail.com>
2022-08-21 18:54:00 +0400

    eh/feat: upload files done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { getMongoConfig } from './configs/mongo.config';
+import { FilesModule } from './files/files.module';
 
 @Module({
 	imports: [
@@ -14,9 +14,9 @@ import { getMongoConfig } from './configs/mongo.config';
 		}),
 		TypegooseModule.forRoot(getMongoConfig(new ConfigService())),
 		AuthModule,
-		TopPageModule,
 		ProductModule,
 		ReviewModule,
+		FilesModule,
 	],
 })
 export class AppModule {}
--- src/top-page/top-page.constants.ts
+++
@@ -1,2 +0,0 @@
-export const NOT_FOUND_PAGE = 'Page with such of id was not found';
-export const NOT_FOUND_BY_ALIAS = 'Page with such of alias doe\'s not exists';
